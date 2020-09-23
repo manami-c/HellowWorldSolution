33f126f1e2fca5fc26ec62b9161c42cae5e61bc2
Manami Crowley <manami.faye@gmail.com>
2020-09-23 14:16:14 -0400

    Add project files.

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
