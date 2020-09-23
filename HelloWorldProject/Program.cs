692cd04ca1371ffc7b28a65fc058b113ac140dd5
Manami Crowley <manami.faye@gmail.com>
2020-09-23 14:28:27 -0400

    Added name and git/sql preferense statenent to be displayed

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -7,6 +7,8 @@ namespace HelloWorldProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Manami");
+            Console.WriteLine("I still don't feel confident with either");
         }
     }
 }
